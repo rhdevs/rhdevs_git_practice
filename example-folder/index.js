99cf670b991b403d2beb15596fdba7ae352744fe
brandoncjh <brandonchongjh@gmail.com>
2020-10-03 00:44:59 +0800

    added JS file

---
+++ example-folder/index.js
@@ -0,0 +1,31 @@
+//Generate dice value and image for player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//Generate dice value and image for player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+//If player 2 wins
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+//If both have the same dice value
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
